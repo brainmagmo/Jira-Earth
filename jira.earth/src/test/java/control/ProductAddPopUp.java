2ea227e9d297f8b3c7ac41d768fd8edc64fcb551
Trevor John Kleinstuber <trevor.kleinstuber@gmail.com>
2023-01-05 18:30:49 -0800

    identifies error in locator

--- jira.earth/src/test/java/control/ProductAddPopUp.java
+++ jira.earth/src/test/java/control/ProductAddPopUp.java
@@ -1,20 +1,18 @@
 package control;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WrapsDriver;
-import org.openqa.selenium.support.FindBy;
-
 import page.CheckoutPage;
 
 public class ProductAddPopUp extends Control {
-	@FindBy(css="#layer_cart a")
-	private WebElement checkoutLink;
 
 	public ProductAddPopUp(WebElement element) {
 		super(element);
 	}
 	
 	public CheckoutPage clickProceedToCheckout() {
+    	WebElement checkoutLink = this.element.findElements(By.cssSelector("#layer_cart a")).get(0);
 		checkoutLink.click();
 		return new CheckoutPage(((WrapsDriver)element).getWrappedDriver());
 	}
--- jira.earth/src/test/java/control/ProductListing.java
+++ jira.earth/src/test/java/control/ProductListing.java
@@ -1,20 +1,20 @@
 package control;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class ProductListing extends Control {
-	@FindBy(xpath="./span[text()='Add to Cart']/../..")
-	private WebElement addToCartButton;
-	@FindBy(id="layer_cart")
-	private WebElement popUp;
 
     public ProductListing(WebElement element) {
         super(element);
     }
 
     public ProductAddPopUp addToCart() {
-
+    	WebElement addToCartButton = this.element.findElements(By.xpath("//span[text()='Add to cart']")).get(0);
+    	addToCartButton.click();
+    	//problem is we're searching the element instead of the page need to cast element to driver
+    	WebElement popUp = this.element.findElement(By.id("layer_cart"));
         return new ProductAddPopUp(popUp); 
     }
 }
