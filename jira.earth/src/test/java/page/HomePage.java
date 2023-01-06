b709b24c51eef855a96d33171d5d08481188cd18
Ernesto Perez Pozo <eperezpozo@knights.ucf.edu>
2023-01-05 20:44:41 -0500

    Add getProductListing() method to HomePage class

--- jira.earth/src/test/java/page/HomePage.java
+++ jira.earth/src/test/java/page/HomePage.java
@@ -1,11 +1,23 @@
 package page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import control.ProductListing;
 
 public class HomePage extends Page {
+
+    @FindBy(how=How.CSS, using="#product-container")
+    private WebElement productListing; 
 
 	public HomePage(WebDriver driver) {
 		super(driver);
 		this.route = "index.php";
+	}
+
+	public ProductListing getProductListing() {
+	    return new ProductListing(this.productListing);
 	}
 }
