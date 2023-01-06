c1a145b057d8bbdb084e26eac6152484ea9c5720
Ernesto Perez Pozo <eperezpozo@knights.ucf.edu>
2023-01-05 20:09:12 -0500

    Add .navigate() and fix tests.

--- jira.earth/src/test/java/InvenautoTests.java
+++ jira.earth/src/test/java/InvenautoTests.java
@@ -8,27 +8,35 @@ public class InvenautoTests extends TestBase {
     public void canTest() {
     	return;
     }
-    
+
     @Test
     public void canOpenIndex() {
     	var expctedURL = "https://invenauto.tech/index.php";
 
 		var selectedURL = fromPages()
 				.getHomePage()
+				.navigate()
 				.getURL();
 
 		assertEquals(selectedURL, expctedURL, "The Home Page should be loaded.");
     }
-    
+
     @Test
     public void canOpenSignInPage() {
     	var expctedURL = "https://invenauto.tech/index.php?controller=authentication&back=my-account";
 
 		var selectedURL = fromPages()
 				.getSigninPage()
+				.navigate()
 				.getURL();
 
 		assertEquals(selectedURL, expctedURL, "The Home Page should be loaded.");    	
     }
 
+//    @Test
+//    public void canCheckoutAfterProductinCart() {
+//        fromPages()
+//          .getHomePage()
+//          .navigate()
+//    }
 }
