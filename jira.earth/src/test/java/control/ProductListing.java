8c294aad204e8636e164c9668e4568cbd0d28556
Trevor John Kleinstuber <trevor.kleinstuber@gmail.com>
2023-01-05 18:34:17 -0800

    identifies needed refactor

--- jira.earth/src/test/java/control/ProductListing.java
+++ jira.earth/src/test/java/control/ProductListing.java
@@ -2,7 +2,7 @@ package control;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.WrapsDriver;
 
 public class ProductListing extends Control {
 
@@ -14,7 +14,9 @@ public class ProductListing extends Control {
     	WebElement addToCartButton = this.element.findElements(By.xpath("//span[text()='Add to cart']")).get(0);
     	addToCartButton.click();
     	//problem is we're searching the element instead of the page need to cast element to driver
-    	WebElement popUp = this.element.findElement(By.id("layer_cart"));
+    	//cast class exception 
+    	//this method should return void and then we should find the popup from HomePage
+    	WebElement popUp = ((WrapsDriver)this.element).getWrappedDriver().findElement(By.id("layer_cart"));
         return new ProductAddPopUp(popUp); 
     }
 }
