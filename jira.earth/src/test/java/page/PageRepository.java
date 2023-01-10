e031bd1ba04873cdba136ee41271f046c3cd7b03
Ernesto Perez Pozo <eperezpozo@knights.ucf.edu>
2023-01-10 10:30:04 -0500

    Refactor PageRepository to not cache

--- jira.earth/src/test/java/page/PageRepository.java
+++ jira.earth/src/test/java/page/PageRepository.java
@@ -4,41 +4,25 @@ import org.openqa.selenium.WebDriver;
 
 public class PageRepository {
 
-	private WebDriver driver;
-	private HomePage homePage;
-	private SigninPage signinPage;
-
-	private AccountPage accountPage;
-
-	private ProductDetailPage ProductDetailPage;
-
+    WebDriver driver;
+    
 	public PageRepository(WebDriver driver) {
 		this.driver = driver;
 	}
 
 	public HomePage getHomePage() {
-		if(this.homePage == null)
-			this.homePage = new HomePage(this.driver);
-		return this.homePage;
+	    return new HomePage(this.driver);
 	}
 
 	public SigninPage getSigninPage() {
-		if(this.signinPage == null)
-			this.signinPage = new SigninPage(this.driver);
-		return this.signinPage;
+	    return new SigninPage(this.driver);
 	}
 
-
 	public AccountPage getAccountPage() {
-		if(this.accountPage == null)
-			this.accountPage = new AccountPage(this.driver);
-		return this.accountPage;
+	    return new AccountPage(this.driver);
 }
 	public ProductDetailPage getProductDetail() {
-		if(this.ProductDetailPage == null)
-			this.ProductDetailPage = new ProductDetailPage(this.driver);
-		return this.ProductDetailPage;
-
+		return new ProductDetailPage(this.driver);
 	}
 
 	public CheckoutPage getCheckoutPage() {
