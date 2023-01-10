bf1169f5145f1ab45b572b0ee2fb95d0fee22563
Ernesto Perez Pozo <eperezpozo@knights.ucf.edu>
2023-01-10 13:58:01 -0500

    Delete canOpen tests
    If the index is changed in the configuration file
    the tests will fail, because it assumes static index.
    Also, they are not related to any features.

--- jira.earth/src/test/java/test/InvenAutoTests.java
+++ jira.earth/src/test/java/test/InvenAutoTests.java
@@ -1,31 +1,9 @@
 package test;
-import static org.testng.Assert.assertEquals;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class InvenAutoTests extends TestBase {
-
-	@Test
-	public void canOpenIndex() {
-		var expctedURL = "https://invenauto.tech/index.php";
-
-		var selectedURL = fromPages().getHomePage().navigate().getURL();
-
-		assertEquals(selectedURL, expctedURL, "The Home Page should be loaded.");
-	}
-
-    @Test
-    public void canOpenSignInPage() {
-    	var expctedURL = "https://invenauto.tech/index.php?controller=authentication&back=my-account";
-
-		var selectedURL = fromPages()
-				.getSigninPage()
-				.navigate()
-				.getURL();
-
-		assertEquals(selectedURL, expctedURL, "The sign-in page should be loaded.");    	
-    }
 
     @Test
     public void fr001B1_canLogin() {
